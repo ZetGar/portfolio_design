daafeec0fa711e0d88f45e6bb76b5888ea1cf58a
ZetGar <88043986+ZetGar@users.noreply.github.com>
2022-01-11 17:06:38 +0900

    Create function_main.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 $(function () {
   // 상단메뉴 고정
-  $("window").scroll(function () {
-    if ($("window").scrollTop() > 0) {
-      $("header").addClass("sticky");
-    } else {
-      $("header").removeClass("sticky");
-    }
+  $(window).scroll(function () {
+    $("header").toggleClass("sticky", window.scrollY > 0);
+    // if ($(this).scrollTop() > 0) {
+    //   $("header").addClass("sticky");
+    // } else {
+    //   $("header").removeClass("sticky");
+    // }
   });
+  // $(window).on("scroll", function () {
+  //   $("header").toggleClass("sticky", window.scrollY > 0);
+  // });
+  // $(document).ready(function () {
+  //   // 상단메뉴 고정
+  //   $("window").scroll(function () {
+  //     alert();
+  //     if ($("window").scrollTop() > 0) {
+  //       $("header").addClass("sticky");
+  //     } else {
+  //       $("header").removeClass("sticky")
+  //     }
+  //   });
+  // });
 });
