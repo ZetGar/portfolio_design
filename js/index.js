32b7761ef82e527c59a850e749d9949830f8e642
ZetGar <88043986+ZetGar@users.noreply.github.com>
2022-01-13 17:17:33 +0900

    Create _common.scss

--- js/index.js
+++ js/index.js
@@ -1,11 +1,36 @@
 $(function () {
+  // 슬라이드 메뉴 배경
   $(window).on("scroll", function () {
     $("header").toggleClass("sticky", window.scrollY > 0);
   });
 
+  // 로고 클릭 시 맨 위로 올라가기
   $("a.logo").click(function () {
     $("html, body").stop().animate({ scrollTop: 0 }, 400);
     return false;
+  });
+
+  // 스크롤 시 애니메이션
+
+  //section.main
+  $(window).load(function () {
+    $(".main").addClass("active");
+  });
+
+  // section.aboutme
+  let $about = $(".aboutme").offset().top - 700;
+  let $port = $(".portfolio").offset().top - 500;
+  let $contact = $("#contact").offset().top - 500;
+  let $footer = $("#contact").offset().top - 800;
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() <= $port) {
+      $(".aboutme").addClass("active");
+    } else if ($(this).scrollTop() <= $contact) {
+      $(".portfolio").addClass("active");
+    } else if ($(this).scrollTop() <= $contact + 300) {
+      $("#contact").addClass("active");
+    }
   });
 });
 
